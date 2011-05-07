11742521f9ca13a439fc41b070c6cc84609d0c4a
Anders <joensson.anders@gmail.com>
2011-05-07 23:33:31 +0200

    changed spec

--- Selenium2Demo/Selenium2Demo/HemnetSpecs.cs
+++ Selenium2Demo/Selenium2Demo/HemnetSpecs.cs
@@ -12,34 +12,40 @@ namespace Selenium2Demo
         [Test]
         public void Yeah()
         {
+            using (var page = OpenPage(@"http://www.hemnet.se/"))
+            {
+                page.FindElement(By.Id("search_item_types_tomt")).Click();
+                page.FindElement(By.Id("search_item_types_villa")).Click();
+                page.FindElement(By.Id("search_item_types_fritidshus")).Click();
+
+                // We must do the selection before interacting with the list, otherwise we will get an error
+                var countyList = page.FindElement(By.Id("search_municipality"));
+                var countyLabel = countyList.FindElement(By.XPath("//label[contains(.,'Håbo kommun')]"));
+                var countyInput = countyLabel.FindElement(By.TagName("input"));
+
+                countyList.Click(); // We must make the "option" visible before clicking on it, otherwise it will fail
+                countyInput.Click(); // Selecting the option
+
+                page.FindElementById("search_submit").Submit();
+
+                var sortByList = new SelectElement(page.FindElement(By.Id("search-results-sort-by")));
+                sortByList.SelectByText("Inkommet");
+
+                var sortOrderList = new SelectElement(page.FindElement(By.Id("search-results-sort-order")));
+                sortOrderList.SelectByText("Nyast först");
+
+                Assert.That("yeah", Is.EqualTo("yeah"));
+            }
+        }
+
+        private RemoteWebDriver OpenPage(string url)
+        {
             var profile = new FirefoxProfile();
-            /*var firebugPath = Path.Combine(Environment.CurrentDirectory, "firebug-1.6.2.xpi");
-            profile.AddExtension(firebugPath);*/
-
+            profile.AddExtension("firebug-1.6.2.xpi");
+            profile.SetPreference("extensions.firebug.currentVersion", "9.99");
             RemoteWebDriver driver = new FirefoxDriver(profile);
-            driver.Navigate().GoToUrl(@"http://www.hemnet.se/");
-
-            driver.FindElement(By.Id("search_item_types_tomt")).Click();
-            driver.FindElement(By.Id("search_item_types_villa")).Click();
-            driver.FindElement(By.Id("search_item_types_fritidshus")).Click();
-
-            // We must do the selection before interacting with the list, otherwise we will get an error
-            var countyList = driver.FindElement(By.Id("search_municipality"));
-            var countyLabel = countyList.FindElement(By.XPath("//label[contains(.,'Håbo kommun')]"));
-            var countyInput = countyLabel.FindElement(By.TagName("input"));
-
-            countyList.Click(); // We must make the "option" visible before clicking on it, otherwise it will fail
-            countyInput.Click(); // Selecting the option
-
-            driver.FindElementById("search_submit").Submit();
-
-            var sortByList = new SelectElement(driver.FindElement(By.Id("search-results-sort-by")));
-            sortByList.SelectByText("Inkommet");
-
-            var sortOrderList = new SelectElement(driver.FindElement(By.Id("search-results-sort-order")));
-            sortOrderList.SelectByText("Nyast först");
-
-            driver.Close();
+            driver.Navigate().GoToUrl(url);
+            return driver;
         }
     }
 }
